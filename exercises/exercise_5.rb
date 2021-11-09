2fd148b0a590344eff5f142279198982bd8ae36a
faez <faezc@hotmail.com>
2021-11-09 20:24:16 +0000

    complete exercise 5 - more queries and the sum and count methods

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.sum(:annual_revenue)
+#4114000
+puts Store.sum(:annual_revenue) / Store.count
+#822800
+puts Store.where("annual_revenue >= ?", 1000000).count
