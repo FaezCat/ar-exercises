ae4ee4af511567fd3377b1c01c42e790a7cb7889
faez <faezc@hotmail.com>
2021-11-09 22:35:56 +0000

    complete error messages example including added validations to stores and employees tables

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Type in a new store name"
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name)
+
+new_store.errors.full_messages.each do |error|
+  puts error
+end
